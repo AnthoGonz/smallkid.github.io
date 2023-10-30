d710a5f4e4f0be1ba69512db3cbcc796d63a4c62
Anthony <anthonygonzalez8102@gmail.com>
2023-10-30 14:37:40 -0600

    Merch

---
+++ mail.php
@@ -0,0 +1,13 @@
+<<?php
+    $myemail = 'howdy@smallkid.band';//<-----Put Your email address here.
+
+    $mail = $_POST['mail'];
+
+    $to = $myemail;
+    $email_subject = "New Merch Subscriber";
+    $email_body = $mail;
+    $headers = "From: $myemail\n";
+    mail($to,$email_subject,$email_body,$headers);
+    //redirect to the 'thank you' page
+    header('Location: index.html');
+    ?>
